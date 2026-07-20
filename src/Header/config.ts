f705c397013712037f48685e15e3e514044f8856
Ali Haider <alikh4837@gmail.com>
2026-07-20 18:25:08 +0500

    feat: update Header configuration to enhance live preview functionality and improve navItems description

--- src/Header/config.ts
+++ src/Header/config.ts
@@ -2,28 +2,40 @@ import type { GlobalConfig } from 'payload'
 
 import { link } from '@/fields/link'
 import { revalidateHeader } from './hooks/revalidateHeader'
+import { generatePreviewPath } from '../utilities/generatePreviewPath'
 
 export const Header: GlobalConfig = {
   slug: 'header',
   access: {
     read: () => true,
   },
+  admin: {
+    livePreview: {
+      url: ({ req }) =>
+        generatePreviewPath({
+          slug: '/',
+          collection: 'pages',
+          req,
+        }),
+    },
+  },
   fields: [
     {
       name: 'navItems',
       type: 'array',
+      admin: {
+        initCollapsed: true,
+        description: 'These links appear left-to-right in the site header, on every page. Drag to reorder.',
+        components: {
+          RowLabel: '@/Header/RowLabel#RowLabel',
+        },
+      },
       fields: [
         link({
           appearances: false,
         }),
       ],
       maxRows: 6,
-      admin: {
-        initCollapsed: true,
-        components: {
-          RowLabel: '@/Header/RowLabel#RowLabel',
-        },
-      },
     },
   ],
   hooks: {
