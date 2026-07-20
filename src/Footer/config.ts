4003555bcd94b2e9d0eec74e11901f035d501c7c
Ali Haider <alikh4837@gmail.com>
2026-07-20 18:25:47 +0500

    feat: enhance Footer configuration with live preview functionality and improved navItems description

--- src/Footer/config.ts
+++ src/Footer/config.ts
@@ -2,28 +2,40 @@ import type { GlobalConfig } from 'payload'
 
 import { link } from '@/fields/link'
 import { revalidateFooter } from './hooks/revalidateFooter'
+import { generatePreviewPath } from '../utilities/generatePreviewPath'
 
 export const Footer: GlobalConfig = {
   slug: 'footer',
   access: {
     read: () => true,
   },
+  admin: {
+    livePreview: {
+      url: ({ req }) =>
+        generatePreviewPath({
+          slug: '/',
+          collection: 'pages',
+          req,
+        }),
+    },
+  },
   fields: [
     {
       name: 'navItems',
       type: 'array',
+      admin: {
+        initCollapsed: true,
+        description: 'These links appear in the site footer, on every page. Drag to reorder.',
+        components: {
+          RowLabel: '@/Footer/RowLabel#RowLabel',
+        },
+      },
       fields: [
         link({
           appearances: false,
         }),
       ],
       maxRows: 6,
-      admin: {
-        initCollapsed: true,
-        components: {
-          RowLabel: '@/Footer/RowLabel#RowLabel',
-        },
-      },
     },
   ],
   hooks: {
