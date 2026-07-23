223832844e71b6c7cec682be8be88a9d22f12ab1
Ali Haider <alikh4837@gmail.com>
2026-07-23 12:30:45 +0500

    refactor: simplify Header component by delegating to HeaderClient

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -30,11 +30,15 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
-        <Link href="/">
+    <header
+      className="sticky top-0 z-20 w-full border-b border-brand-line/60 bg-background/80 backdrop-blur-md"
+      {...(theme ? { 'data-theme': theme } : {})}
+    >
+      <div className="container flex items-center justify-between py-4">
+        <Link href="/" className="shrink-0">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
         </Link>
+
         <HeaderNav data={data} />
       </div>
     </header>
